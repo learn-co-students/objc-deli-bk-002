497a85b7fc967ee5cefe67e51299bd69d433fb48
azmuth94 <dannyaksenov@gmail.com>
2015-08-21 21:27:39 -0400

    next in line please

--- Objc-deli/FISAppDelegate.m
+++ Objc-deli/FISAppDelegate.m
@@ -16,10 +16,29 @@
     return YES;
 }
 
-/*
- 
- *  Define your methods here!
- 
- */
+- (NSString *) stringWithDeliLine:(NSArray *)deliLine {
+    NSString *line = @"The line is:";
+    NSMutableString *mLine = [line mutableCopy];
+    if ([deliLine count] == 0) {
+        return @"The line is currently empty.";
+
+    } else {
+        for (NSUInteger i = 0; i < [deliLine count]; i++) {
+            [mLine appendFormat:@"\n%lu. %@",(i+1),deliLine[i]];
+        }
+    }
+    return mLine;
+}
+
+- (void) addName:(NSString *)name toDeliLine:(NSMutableArray *)deliLine {
+    [deliLine addObject:name];
+}
+
+- (NSString *) serveNextCustomerInDeliLine:(NSMutableArray *)deliLine {
+    NSString *nextCustomer = deliLine[0];
+    [deliLine removeObjectAtIndex:0];
+    return nextCustomer;
+}
+
 
 @end
